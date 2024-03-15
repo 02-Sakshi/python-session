a7c648af3f0a7cc5728ed382224e2ff9804317e9
02-Sakshi <aids.23.sakshichougule@gmail.com>
2024-03-15 17:30:08 +0530

    first commit

---
+++ cond.py
@@ -0,0 +1,10 @@
+attendance = int(input("Enter your attendance percentage :"))
+
+if attendance > 75:
+   print("Attendance is good")
+
+elif attendance > 30:
+     print("You are in defaulter list")
+
+else:
+    print("Leave college")
